2e3e077b0fa8fd0124b54c71d78c95a916ca6d82
Joel Cano <canito0890@gmail.com>
2016-02-04 13:10:40 -0600

    engine support

--- lib/active_admin/resource/controllers.rb
+++ lib/active_admin/resource/controllers.rb
@@ -6,7 +6,9 @@ module ActiveAdmin
       # Returns a properly formatted controller name for this
       # config within its namespace
       def controller_name
-        [namespace.module_name, resource_name.plural.camelize + "Controller"].compact.join('::')
+        base = [namespace.module_name, resource_name.plural.camelize + "Controller"]
+        base.unshift(namespace.application.engine.parent.to_s) if namespace.application.engine.present?
+        base.compact.join('::')
       end
 
       # Returns the controller for this config
